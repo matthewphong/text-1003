a2190664c21dfc9941470d8c59d5f5cf5df01db0
matthewphong <99379552+matthewphong@users.noreply.github.com>
2022-03-10 11:18:49 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+from flask import Flask, request, render_template
+from textblob import TextBlob
+
+
+# In[12]:
+
+
+app = Flask(__name__)
+
+
+# In[13]:
+
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        t = request.form.get("text")
+        print(t)
+        r = TextBlob(t).sentiment
+        return(render_template("index.html", results = r))
+    else:
+        return(render_template("index.html", results = "2"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
